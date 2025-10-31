532b27f159e2f7123dd4906fcfabd2388c62b3e4
Smiley98 <smileysofficialcinema@gmail.com>
2025-10-31 09:31:54 -0400

    Added challenges

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -51,7 +51,7 @@ void Example2()
 		{
 			// Convert from [0, 512] to [0, 1]
 			float u = x / (float)CPU_IMAGE_SIZE;
-
+			
 			// s3d expects colors within the range of [0, 255], so convert!
 			Color color = BLACK;
 			color.r = u * 255.0f;
@@ -68,22 +68,24 @@ void Example3()
 	{
 		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
 		{
+			// Best to use vector-math to do component-wise operations!
 			Vector2 uv = { x, y };
 			uv /= CPU_IMAGE_SIZE;
 
+			// Automatically convert vec2/vec3/vec4 to colour when you're done!
 			Color color = Float2ToColor(&uv.x);
 			SetPixel(&gImageCPU, x, y, color);
-			// Render a 2D gradient.
-			// Use a Vector2 to calculate uv!
 		}
 	}
 }
 
+// Challenge 1 (medium) -- render the following fading 2d gradient: https://www.shadertoy.com/view/MXKcR3
 void Example4()
 {
 
 }
 
+// Challenge 2 (impossible) -- render the animating ring: https://www.shadertoy.com/view/X3tBDs
 void Example5()
 {
 
