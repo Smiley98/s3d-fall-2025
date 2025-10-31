dbeafaab931f50b9efac860a8ba79a003a1e86c5
Smiley98 <smileysofficialcinema@gmail.com>
2025-10-31 12:42:45 -0400

    Added 1d gradient start

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -4,22 +4,33 @@
 #include "Camera.h"
 #include "Time.h"
 
-static Image fDiffuseMap;
+void Example1();
+void Example2();
+void Example3();
+void Example4();
+void Example5();
 
 void RasterizationScene::OnLoad()
 {
-	gCamera = FromView(LookAt({ 0.0f, 0.0f, 5.0f }, V3_ZERO, V3_UP));
-	CreateImageFromFile(&fDiffuseMap, "assets/textures/african_head_diffuse.png", true);
 }
 
 void RasterizationScene::OnUnload()
 {
-	DestroyImage(&fDiffuseMap);
 }
 
 void RasterizationScene::OnUpdate(float dt)
 {
 	ClearColor(&gImageCPU, BLACK);
+	//Example1();
+	Example2();
+	//Example3();
+	//Example4();
+	//Example5();
+}
+
+void Example1()
+{
+	// Solid colour image
 	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
 	{
 		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
@@ -28,4 +39,37 @@ void RasterizationScene::OnUpdate(float dt)
 			SetPixel(&gImageCPU, x, y, color);
 		}
 	}
+}
+
+void Example2()
+{
+	// 1D gradient -- https://www.shadertoy.com/view/43Kyzc
+	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
+	{
+		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
+		{
+			// Modify this to be the horizontal position of the pixel (x) divided by the image size (CPU_IMAGE_SIZE)
+			float u = 0.0f;
+
+			Color color = BLACK;
+			color.r = u * 255.0f;
+
+			SetPixel(&gImageCPU, x, y, color);
+		}
+	}
+}
+
+void Example3()
+{
+
+}
+
+void Example4()
+{
+
+}
+
+void Example5()
+{
+
 }
