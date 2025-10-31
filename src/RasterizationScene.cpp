3c5146b4c7dd207ef2f60f968c7ceadd976deea7
Smiley98 <smileysofficialcinema@gmail.com>
2025-10-31 12:34:14 -0400

    Added solid colour shader

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -20,4 +20,12 @@ void RasterizationScene::OnUnload()
 void RasterizationScene::OnUpdate(float dt)
 {
 	ClearColor(&gImageCPU, BLACK);
+	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
+	{
+		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
+		{
+			Color color = BLUE;
+			SetPixel(&gImageCPU, x, y, color);
+		}
+	}
 }
