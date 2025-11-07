3232bd55c3aba4e1014f3a76f686e46693c4f513
Smiley98 <smileysofficialcinema@gmail.com>
2025-11-07 09:38:23 -0500

    Added homework 4 start

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -4,15 +4,11 @@
 #include "Camera.h"
 #include "Time.h"
 
-float sdCircle(Vector2 p, float r)
-{
-	return Length(p) - r;
-}
+float sdCircle(Vector2 p, float r);
+float step(float edge, float x);
 
-float step(float edge, float x)
-{
-	return x < edge ? 0.0f : 1.0f;
-}
+void Example1();
+void Example2();
 
 void RasterizationScene::OnLoad()
 {
@@ -24,8 +20,35 @@ void RasterizationScene::OnUnload()
 
 void RasterizationScene::OnUpdate(float dt)
 {
-	// Challenge: render a circle using an SDF (https://www.shadertoy.com/view/XXtfDn)
 	ClearColor(&gImageCPU, BLACK);
+	//Example1();
+	//Example2();
+
+	// Example function call from Rasterization.h --> Should render a horizontal line the width of your screen if implemented correctly!
+	DrawLineX(&gImageCPU, 100, 0, 511, RED);
+
+	// Homework:
+	// Draw a border around your screen using DrawRectLines
+	// Draw 2 horizontal lines
+	// Draw 2 vertical lines
+	// Draw a rectangle in the centre of your screen and outline it
+	// Draw a circle at your mouse cursor and outline it
+	// Draw a line connecting the above two shapes
+}
+
+// Fast way of rendering a circle -- "rasterization" - instead of evaluating every pixel, only evaluate the pixel's within the circle!
+// Moreover, the evaluation is extremely cheap since its just a bunch of different-sized scanlines
+void Example2()
+{
+	// Renders at 60fps on Smiley's PC
+	DrawCircle(&gImageCPU, 256, 256, 255, MAGENTA);
+}
+
+// Slow way of rendering a circle -- evaluated for every pixel even if the pixel is not within the circle!
+// Moreover, the evaluation is extremely expensive given the square-root calculation in the circle's SDF.
+void Example1()
+{
+	// Renders at 30fps on Smiley's PC
 	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
 	{
 		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
@@ -41,4 +64,14 @@ void RasterizationScene::OnUpdate(float dt)
 			SetPixel(&gImageCPU, x, y, color);
 		}
 	}
+}
+
+float step(float edge, float x)
+{
+	return x < edge ? 0.0f : 1.0f;
+}
+
+float sdCircle(Vector2 p, float r)
+{
+	return Length(p) - r;
 }
