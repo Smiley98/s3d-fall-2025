a6c902a4362f3e9a0d7b684dee18f0e4dd754350
Smiley98 <smileysofficialcinema@gmail.com>
2025-11-07 13:05:22 -0500

    Added circle challenge start

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -6,9 +6,6 @@
 
 void Example1();
 void Example2();
-void Example3();
-void Example4();
-void Example5();
 
 void RasterizationScene::OnLoad()
 {
@@ -21,21 +18,20 @@ void RasterizationScene::OnUnload()
 void RasterizationScene::OnUpdate(float dt)
 {
 	ClearColor(&gImageCPU, BLACK);
-	//Example1();
+	Example1();
 	//Example2();
-	Example3();
-	//Example4();
-	//Example5();
 }
 
+// Challenge: Port the following shader to s3d: https://www.shadertoy.com/view/XXtfDn
+// If you want to use glsl's step function, you'll also need to port that to s3d:
+// https://registry.khronos.org/OpenGL-Refpages/gl4/html/step.xhtml
 void Example1()
 {
-	// Solid colour image
 	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
 	{
 		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
 		{
-			Vector3 rgb = { 0.5f, 0.0f, 1.0f };
+			Vector3 rgb = V3_ONE;
 
 			Color color = Float3ToColor(&rgb.x);
 			SetPixel(&gImageCPU, x, y, color);
@@ -45,63 +41,4 @@ void Example1()
 
 void Example2()
 {
-	// 1D gradient -- https://www.shadertoy.com/view/43Kyzc
-	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
-	{
-		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
-		{
-			// Modify this to be the horizontal position of the pixel (x) divided by the image size (CPU_IMAGE_SIZE)
-			float u = x / (float)CPU_IMAGE_SIZE;
-
-			Color color = BLACK;
-			color.r = u * 255.0f;
-
-			SetPixel(&gImageCPU, x, y, color);
-		}
-	}
-}
-
-void Example3()
-{
-	// 2D gradient -- https://www.shadertoy.com/view/43Kyzc
-	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
-	{
-		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
-		{
-			// Calculate uv by dividing the xy position of the pixel by image size
-			Vector2 uv = { x, y };
-			uv /= CPU_IMAGE_SIZE;
-
-			Color color = Float2ToColor(&uv.x);
-			SetPixel(&gImageCPU, x, y, color);
-		}
-	}
-}
-
-// Challenge 1 - Fading gradient
-void Example4()
-{
-	// Time since s3d start
-	float t = TotalTime();
-
-	// Fading 2D gradient -- https://www.shadertoy.com/view/MXKcR3
-	for (int y = 0; y < CPU_IMAGE_SIZE; y++)
-	{
-		for (int x = 0; x < CPU_IMAGE_SIZE; x++)
-		{
-			// Blend between red-green gradient and blue-purple gradient
-			Vector2 uv = { x, y };
-			uv /= CPU_IMAGE_SIZE;
-
-			Color color = Float2ToColor(&uv.x);
-			SetPixel(&gImageCPU, x, y, color);
-		}
-	}
-}
-
-// Challenge 2 -- Animating ring
-void Example5()
-{
-	// Ultra-difficult task: make the "Ring of PTSD" -- https://www.shadertoy.com/view/X3tBDs
-	// You'll need to make glsl functions such as step yourself since they're not part of C++!
 }
