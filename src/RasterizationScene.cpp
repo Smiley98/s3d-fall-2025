372d7f51a09911335af9a2f71f857f2fbfaa86c6
Smiley98 <smileysofficialcinema@gmail.com>
2025-11-14 09:09:22 -0500

    Added homework 4 solution

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -1,6 +1,7 @@
 #include "RasterizationScene.h"
 #include "Rasterization.h"
 #include "ImageUtils.h"
+#include "Window.h"
 #include "Camera.h"
 #include "Time.h"
 
@@ -17,22 +18,32 @@ void RasterizationScene::OnUpdate(float dt)
 	Image* img = &gImageCPU;
 	ClearColor(img, BLACK);
 
+	// Draw 2 horizontal lines
 	DrawLineX(img, 100, 100, 200, RED);
 	DrawLineX(img, 200, 200, 300, RED);
 
+	// Draw 2 vertical lines
 	DrawLineY(img, 100, 100, 200, GREEN);
 	DrawLineY(img, 200, 200, 300, GREEN);
 
-	DrawRect(img, 300, 300, 100, 100, BLUE);
-	DrawRectLines(img, 299, 299, 101, 101, MAGENTA);
-
-	DrawLine(img, 0, 0, CPU_IMAGE_SIZE - 1, CPU_IMAGE_SIZE - 1, YELLOW);
-
-	// Homework:
-	// Draw a border around your screen using DrawRectLines
-	// Draw 2 horizontal lines
-	// Draw 2 vertical lines
 	// Draw a rectangle in the centre of your screen and outline it
+	DrawRect(img, 252, 252, 8, 8, BLUE);
+	DrawRectLines(img, 251, 251, 9, 9, MAGENTA);
+
+	// Draw a border around your screen using DrawRectLines
+	DrawRectLines(img, 0, 0, CPU_IMAGE_SIZE - 1, CPU_IMAGE_SIZE - 1, CYAN);
+	
 	// Draw a circle at your mouse cursor and outline it
-	// Draw a line connecting the above two shapes
+	Vector2 mouse = GetMousePosition();
+	mouse.x = Remap(mouse.x, 0.0f, SCREEN_WIDTH,  0.0f, CPU_IMAGE_SIZE);
+	mouse.y = Remap(mouse.y, SCREEN_HEIGHT, 0.0f, 0.0f, CPU_IMAGE_SIZE);
+
+	int r = 5;
+	Vector2 min{ (float)r, (float)r };
+	Vector2 max{ CPU_IMAGE_SIZE - (r + 1), CPU_IMAGE_SIZE - (r + 1) };
+	mouse = Clamp(mouse, min, max);
+	DrawCircle(img, mouse.x, mouse.y, r, GRAY);
+	
+	// Draw a line connecting the centre rectangle to the cursor circle
+	DrawLine(img, 255, 255, mouse.x, mouse.y, YELLOW);
 }
